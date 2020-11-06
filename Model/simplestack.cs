f23d50e9a20d2ea4f39e3e529088361747ec175d
Sarklai <andv10@mail.ru>
2020-11-06 14:40:28 +0300

    v1

---
+++ Model/simplestack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace stack_test.Model
+{
+    public class simplestack <T>
+    {
+        private List<T> items = new List<T>();
+
+        public int Count => items.Count;
+
+        public bool IsEmpty => items.Count == 0;
+
+        public void Push(T item)    // Операция ввода
+        {
+            items.Add(item);
+        }
+
+        public T Pop()  // Операция вывода
+        {
+            if (!IsEmpty)
+            {
+                var item = items.LastOrDefault();
+                items.Remove(item);
+                return item;
+            }
+            else
+            {
+                throw new NullReferenceException("Пустой стек");
+            }
+        }
+
+        public T Peek() // Пик
+        {
+            if (!IsEmpty)
+            {
+                return items.LastOrDefault();
+            }
+            else
+            {
+                throw new NullReferenceException("Пустой стек");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Стек с {Count} элементами";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using stack_test.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stack_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var simplestack = new simplestack<int>();
+
+            simplestack.Push(12);
+            simplestack.Push(74);
+            simplestack.Push(31);
+
+            var item = simplestack.Pop();
+            var item2 = simplestack.Peek();
+
+            Console.WriteLine(item);
+            Console.WriteLine(item2);
+            Console.ReadLine();
+        }
+    }
+}
